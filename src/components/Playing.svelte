217dcb25e88f3388707ba7fe6ddc8d86f3a06024
vorillaz <theodorevorillas@gmail.com>
2024-03-23 23:14:29 +0200

    feat: new components

---
+++ src/components/Playing.svelte
@@ -0,0 +1,23 @@
+<script>
+  const fetchSpotify = (async () => {
+    const response = await fetch("/api/spotify");
+    return await response.json();
+  })();
+</script>
+
+{#await fetchSpotify then data}
+  {#if data && data.title}
+    <span class="flex gap-half center">
+      <span class="bars">
+        <span class="bar" />
+        <span class="bar" />
+        <span class="bar" />
+      </span>
+      <span>
+        Now listening to {data.title} by {data.artist}
+      </span>
+    </span>
+  {/if}
+{:catch}
+  ds
+{/await}
