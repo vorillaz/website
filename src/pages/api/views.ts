10433ba867a9d411bc6938ff7e0923e6ac9bbf8b
vorillaz <theodorevorillas@gmail.com>
2024-03-24 00:03:28 +0200

    fix: add ttl for views

--- src/pages/api/views.ts
+++ src/pages/api/views.ts
@@ -50,7 +50,7 @@ export const GET: APIRoute = async ({ request }) => {
       status: 200,
       headers: {
         "content-type": "application/json",
-        "cache-control": "public, s-maxage=60, stale-while-revalidate=25",
+        "cache-control": "no-store, max-age=0",
       },
     }
   );
