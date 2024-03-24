741d772cdbc06bc5ad785229810f649fd4ee2817
vorillaz <theodorevorillas@gmail.com>
2024-03-24 22:21:02 +0200

    fix: spotify caching

--- src/pages/api/spotify.ts
+++ src/pages/api/spotify.ts
@@ -98,7 +98,7 @@ export const GET: APIRoute = async () => {
         status: 200,
         headers: {
           "content-type": "application/json",
-          "cache-control": "public, s-maxage=60, stale-while-revalidate=25",
+          "cache-control": "no-store, max-age=0",
         },
       }
     );
