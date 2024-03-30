4f3a0dc1044a81b54c446a4d537b1eebcb477b20
vorillaz <theodorevorillas@gmail.com>
2024-03-30 23:02:08 +0200

    fix: img component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
+import { defineConfig, squooshImageService } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import vercel from "@astrojs/vercel/serverless";
 import { rehypePlugins, remarkPlugins } from "./config/plugins";
@@ -10,6 +10,9 @@ import db from "@astrojs/db";
 export default defineConfig({
   site: "https://vorillaz.com",
   prefetch: true,
+  image: {
+    service: squooshImageService(),
+  },
   markdown: {
     smartypants: true,
     drafts: true,
