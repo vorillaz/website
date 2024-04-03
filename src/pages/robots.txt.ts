df5ec35f5f51180d5722f53e99b6248671ea5153
vorillaz <theodorevorillas@gmail.com>
2024-04-03 19:46:50 +0300

    fix: robots

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,16 +1,16 @@
 import type { APIRoute } from "astro";
 
-const robots = `
-User-agent: Googlebot
-Disallow: /nogooglebot/
-
+const robotsTxt = `
 User-agent: *
 Allow: /
 
-Sitemap: ${new URL("sitemap-index.xml", "vorillaz.com").href}
+Sitemap: ${new URL("sitemap-index.xml", import.meta.env.SITE).href}
 `.trim();
 
-export const GET: APIRoute = () =>
-  new Response(robots, {
-    headers: { "Content-Type": "text/plain" },
+export const GET: APIRoute = () => {
+  return new Response(robotsTxt, {
+    headers: {
+      "Content-Type": "text/plain; charset=utf-8",
+    },
   });
+};
